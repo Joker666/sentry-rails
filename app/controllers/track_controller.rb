ea7a72cd2d8b3b45b757885f3a1f35f27d07ba29
Hasan Rafi <mr.k779@outlook.com>
2020-11-27 01:52:10 +0600

    Update order

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -4,21 +4,22 @@ class TrackController < ApplicationController
         case id
             when 1
                 1 / 0
+            
             when 2
-                Raven.capture_message('Something went very wrong', {
-                    extra: { 'key' => 'value' },
-                    backtrace: caller
-                })
-            when 3
                 Raven.capture do
                     1 / 0
                 end
-            when 4
+            when 3
                 begin
                     1 / 0
                 rescue ZeroDivisionError => exception
                     Raven.capture_exception(exception)
                 end
+            when 4
+                Raven.capture_message('Something went very wrong', {
+                    extra: { 'key' => 'value' },
+                    backtrace: caller
+                })
             end
         render json: params[:id]
     end
